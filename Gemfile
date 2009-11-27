a9a624532fce7a279bc32be51dd9a5746294337a
Gabor Veszi <veszig@done.hu>
2009-11-27 09:23:32 +0100

    bundler support

---
+++ Gemfile
@@ -0,0 +1,21 @@
+clear_sources
+source 'http://gemcutter.org'
+bundle_path 'vendor/bundler_gems'
+
+gem 'rails'
+
+gem 'haml'
+gem 'RedCloth'
+gem 'formtastic'
+gem 'railhead_autouser'
+gem 'searchlogic'
+
+only :test do
+  gem 'rspec', :require_as => 'spec'
+  gem 'rspec-rails', :require_as => 'spec/rails'
+  gem 'cucumber'
+  gem 'machinist'
+  gem 'faker'
+  gem 'webrat'
+  gem 'nokogiri'
+end
--- config/environment.rb
+++ config/environment.rb
@@ -2,12 +2,18 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 
+Rails::Initializer.module_eval do
+  def load_gems
+    Bundler.require_env RAILS_ENV
+  end
+end
+
 Rails::Initializer.run do |config|
-  config.gem 'haml'
-  config.gem 'RedCloth'
-  config.gem 'formtastic'
-  config.gem 'railhead_autouser'
-  config.gem 'searchlogic'
+  # config.gem 'haml'
+  # config.gem 'RedCloth'
+  # config.gem 'formtastic'
+  # config.gem 'railhead_autouser'
+  # config.gem 'searchlogic'
 
   config.time_zone = 'Budapest'
   config.i18n.default_locale = :hu
